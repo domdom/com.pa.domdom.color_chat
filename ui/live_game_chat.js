aa91a1f97c19910887e8ba6c79b324d8b01a30e5
domdom <domdom314@gmail.com>
2015-04-07 13:12:04 +1000

    get mod ready for release

--- ui/live_game_chat.js
+++ ui/live_game_chat.js
@@ -1,7 +1,7 @@
 
-$('span.chat_message_player_name').attr( 'data-bind', $('span.chat_message_player_name').attr( 'data-bind') + ", style: (isGlobal() && model.color_map()[player_name] ? model.color_map()[player_name] : '')");
+//$('span.chat_message_player_name').attr( 'data-bind', $('span.chat_message_player_name').attr( 'data-bind') + ", style: (isGlobal() && model.color_map()[player_name] ? model.color_map()[player_name] : '')");
 
-var elem = '<span style="width:10px;height:3px;margin-bottom:2px;display:inline-block" data-bind="visible: isGlobal, style: {backgroundColor: (isGlobal() && model.color_map()[player_name] ? model.color_map()[player_name].color : \'\')}"></span>'
+var elem = '<span style="line-height:20px;margin:0;width:3px;height:11px;margin-bottom:-1px;display:inline-block" data-bind="visible: isGlobal, style: {backgroundColor: (isGlobal() && model.color_map()[player_name] ? model.color_map()[player_name].color : \'\')}"></span>'
 
 $('span.chat_message_player_name').parent().prepend(elem);
 
