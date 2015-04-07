e2d03eaa63990518c5549ced097343a1d462bc83
domdom <domdom314@gmail.com>
2015-04-07 11:44:33 +1000

    Add dot

--- ui/live_game_chat.js
+++ ui/live_game_chat.js
@@ -1,5 +1,9 @@
 
-$('span.chat_message_player_name').attr( 'data-bind', $('span.chat_message_player_name').attr( 'data-bind') + ", style: {color: isGlobal() && model.color_map()[player_name] ? model.color_map()[player_name] : ''}");
+$('span.chat_message_player_name').attr( 'data-bind', $('span.chat_message_player_name').attr( 'data-bind') + ", style: (isGlobal() && model.color_map()[player_name] ? model.color_map()[player_name] : '')");
+
+var elem = '<span style="width:10px;height:3px;margin-bottom:2px;display:inline-block" data-bind="visible: isGlobal, style: {backgroundColor: (isGlobal() && model.color_map()[player_name] ? model.color_map()[player_name].color : \'\')}"></span>'
+
+$('span.chat_message_player_name').parent().prepend(elem);
 
 model.color_map = ko.observable({});
 handlers.update_player_color_index = function(payload) {
