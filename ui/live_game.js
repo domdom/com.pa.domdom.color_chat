68918f6fc82d650c57e5d996b89f9669c74b7ab6
domdom <domdom314@gmail.com>
2016-01-09 12:12:01 +1000

    update for server messages

--- ui/live_game.js
+++ ui/live_game.js
@@ -7,7 +7,10 @@ model.player_color_index = ko.computed(function() {
 			var c = players[p].primary_color.map(function(val) {
 				return Math.min(255, val);
 			})
-			primary_map[players[p].slots[s]] = {color: 'rgb(' + c.join(',') + ')', backgroundColor: 'rgba(255,255,255,0.0)'};
+			primary_map[players[p].slots[s]] = {
+				color: 'rgb(' + c.join(',') + ')',
+				backgroundColor: 'rgba(255,255,255,0.0)'
+			};
 		}
 	}
 	api.panels.chat.message('update_player_color_index', primary_map);
--- ui/live_game_chat.js
+++ ui/live_game_chat.js
@@ -1,9 +1,32 @@
 
-//$('span.chat_message_player_name').attr( 'data-bind', $('span.chat_message_player_name').attr( 'data-bind') + ", style: (isGlobal() && model.color_map()[player_name] ? model.color_map()[player_name] : '')");
+//$('span.chat_message_player_name').attr( 'data-bind', $('span.chat_message_player_name').attr( 'data-bind') + ", style: ((isGlobal() || isServer()) && model.color_map()[player_name] ? model.color_map()[player_name] : '')");
 
-var elem = '<span style="line-height:20px;margin:0;width:3px;height:11px;margin-bottom:-1px;display:inline-block" data-bind="visible: isGlobal, style: {backgroundColor: (isGlobal() && model.color_map()[player_name] ? model.color_map()[player_name].color : \'\')}"></span>'
+var elem = '<span style="line-height:20px;margin:0;width:3px;height:11px;margin-bottom:-1px;display:inline-block" data-bind="visible: (isGlobal() || isServer()), style: {backgroundColor: $root.getMessageFlairColor($data)}"></span>'
 
 $('span.chat_message_player_name').parent().prepend(elem);
+
+$('.server_message span.chat_message_body').parent().prepend(elem);
+
+model.getMessageFlairColor = function(data) {
+	var color_map = model.color_map();
+
+	if (data.isServer()) {
+		var message = data.message;
+
+		for (var i = message.length; i >= 0; i--) {
+			var _name = message.substring(0, i);
+			if (_name in color_map) {
+				return color_map[_name].color;
+			}
+		}
+	}
+	if (data.isGlobal()) {	
+		if (data.player_name in color_map) {
+			return color_map[data.player_name].color;
+		}
+	}
+	return '';
+}
 
 model.color_map = ko.observable({});
 handlers.update_player_color_index = function(payload) {
